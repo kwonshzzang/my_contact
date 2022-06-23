95fa5313eac57f4aea34fa37a983d00421b85502
kwonshzzang <105833021+kwonshzzang@users.noreply.github.com>
2022-06-23 10:32:51 +0900

    초기 프로젝트

---
+++ src/test/java/kr/co/kwonshzzang/mycontact/MyContactApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kwonshzzang.mycontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyContactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
